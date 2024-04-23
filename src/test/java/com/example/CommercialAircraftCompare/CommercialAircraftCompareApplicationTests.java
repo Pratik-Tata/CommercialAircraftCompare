7ac59d97723e861c81ccec7580c5824d72c5c2a6
Pratik-Tata <pratiktata123@live.com>
2024-04-23 16:58:27 +0530

    initial

---
+++ src/test/java/com/example/CommercialAircraftCompare/CommercialAircraftCompareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CommercialAircraftCompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercialAircraftCompareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
