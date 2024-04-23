d629126a77d75cfd349438c6b1fb3f5f24ff3f46
Pratik-Tata <pratiktata123@live.com>
2024-04-23 21:01:56 +0530

    Manufacturer class -- incomplete

---
+++ src/main/java/com/example/CommercialAircraftCompare/Model/Manufacturer.java
@@ -0,0 +1,10 @@
+package com.example.CommercialAircraftCompare.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Manufacturer {
+    @Getter @Setter private int id_internal;
+    @Getter @Setter private String makeName;
+    @Getter @Setter private String CountryOfOrigin;
+}
