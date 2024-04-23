f3732aa578bb25b101a0bc3050437d8db3218b35
Pratik-Tata <pratiktata123@live.com>
2024-04-23 21:12:01 +0530

    added year of origin

--- src/main/java/com/example/CommercialAircraftCompare/Model/Manufacturer.java
+++ src/main/java/com/example/CommercialAircraftCompare/Model/Manufacturer.java
@@ -6,5 +6,13 @@ import lombok.Setter;
 public class Manufacturer {
     @Getter @Setter private int id_internal;
     @Getter @Setter private String makeName;
-    @Getter @Setter private String CountryOfOrigin;
+    @Getter @Setter private String countryOfOrigin;
+    @Getter @Setter private int yearOfOrigin;
+
+    public Manufacturer(int id_internal, String makeName, String countryOfOrigin, int yearOfOrigin) {
+        this.id_internal = id_internal;
+        this.makeName = makeName;
+        this.countryOfOrigin = countryOfOrigin;
+        this.yearOfOrigin = yearOfOrigin;
+    }
 }
