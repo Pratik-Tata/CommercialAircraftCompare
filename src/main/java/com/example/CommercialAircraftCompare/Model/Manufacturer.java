96b7db8a0ee034d4edf45079c014afc2fffbb985
Pratik-Tata <pratiktata123@live.com>
2024-04-26 01:11:07 +0530

    added aircraft class and made some changes to manufacturer class

--- src/main/java/com/example/CommercialAircraftCompare/Model/Manufacturer.java
+++ src/main/java/com/example/CommercialAircraftCompare/Model/Manufacturer.java
@@ -1,15 +1,19 @@
 package com.example.CommercialAircraftCompare.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import java.time.Year;
+
 
 public class Manufacturer {
     @Getter @Setter private int id_internal;
     @Getter @Setter private String makeName;
     @Getter @Setter private String countryOfOrigin;
-    @Getter @Setter private int yearOfOrigin;
+    @Getter @Setter private Year yearOfOrigin;
 
-    public Manufacturer(int id_internal, String makeName, String countryOfOrigin, int yearOfOrigin) {
+    public Manufacturer(int id_internal, String makeName, String countryOfOrigin, Year yearOfOrigin) {
         this.id_internal = id_internal;
         this.makeName = makeName;
         this.countryOfOrigin = countryOfOrigin;
